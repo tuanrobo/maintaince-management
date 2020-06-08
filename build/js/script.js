fe024cff13afc1fd6a8e3ffadbfacdea1d65b90a
tuanrobo <tuanrobo@gmail.com>
2020-06-08 11:18:28 +0700

    update

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,57 @@
-// var fs  = require("fs");
-// var f = fs.readFileSync('./../spares-list.json').toString();
-// var pages = JSON.parse(f);
-// for (var key in pages) {
-//     var page = pages[key];
-//     fs.writeFile(
-//         key.replace(/ /g, '_')+'.html',
-//         '<h1>'+page.title+'</h1>'
-//         + '<p>'+page.description+'</p>'
-//     );
-// }
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+$(".btn-group > .btn").click(function(){
+    $(".btn-group > .btn").removeClass("active");
+    $(this).addClass("active");
+});
+
+/* Set the width of the side navigation to 250px */
+var ele = document.getElementById("showSidebar");
+function openNav() {
+    ele.style.display = "block";
+    ele.classList.add('sidebar-sticky')    
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    ele.style.display = "none";    
+    document.body.style.backgroundColor = "transparent";
+  }
